05f6aca20f536fcb0f4929c8e52c3c889a9cd6c9
HackxTrick <153048345+HackxTrick@users.noreply.github.com>
2024-03-11 11:49:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 20488530
+API_HASH     = "d15591df8f332e3c0bc64ca09a1c8842"
+BOT_TOKEN    = "7047634502:AAFc5Z5NATeVnB6Hae2qMSHIc3-EqXxeY8g"
+SESSION      = "BQE4oVIAG5IM2-ZsrazqHCp_tF1npe4nuBKApM0r4Wpz-7gaC9lnoBbhrQrYLBH3HeovsYifGc6LEiC3UgcKD6t2MNTaYNBt7Xn4S-CRwjmEHXoNlcOULOWNroXWW_I_9h8vKVw0HHraRCk6Ux7yqsdL87WU8jU5vRtMWyxwWDRl7X8sqUTm1BkG90wgT-Nz_oN2eL0FGMylq1lr-_cL7dP2m1M_iiMxnqnEMxgSsNQcWdNJ2nl-KOylw7QtAO__nteQwd8yBxRjHz2dYXiml8yjAUhiryxC-cc490RakIJQZPoea9YJCldoAuSMKFtPEM0LlF-WGrxx22H6ZeGTq6AiGtc8ggAAAAGXX_pYAA"
+DATABASE_URI = "mongodb+srv://Hackx:Hackx@hackx.6flteju.mongodb.net/?retryWrites=true&w=majority&appName=Hackx"
+LOG_CHANNEL  = -1002007169772
+ADMIN        = 1910728581
+CHANNEL      = "@GAURAV_BOTS"
